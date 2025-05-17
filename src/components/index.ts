af8e231332e01ed6e7cfd51a817be4ecb4c8b399
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-05-17 22:43:11 +0530

    Pagination Component exported

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as Card } from "./Card";
 export { default as DatePicker } from "./DatePicker";
 export { default as Input } from "./Input";
+export { default as Pagination } from "./Pagination";
