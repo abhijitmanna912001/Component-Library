97b8f765a4ce9dcc264961a2c3a17b38ae4c611e
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-05-17 11:20:10 +0530

    DatePicker Stories Completed

---
+++ src/stories/DatePicker.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryObj } from "@storybook/react";
+import DatePicker from "../components/DatePicker";
+
+const meta: Meta<typeof DatePicker> = {
+  title: "Components/DatePicker",
+  component: DatePicker,
+  parameters: { layout: "centered" },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    selectedDate: new Date(),
+    onChange: (date: string) => console.log(date),
+  },
+};
+
+export const WithCustomSize: Story = {
+  args: {
+    selectedDate: new Date(),
+    onChange: (date: string) => console.log(date),
+    size: "sm",
+  },
+};
+
+export const WithCustomColor: Story = {
+  args: {
+    selectedDate: new Date(),
+    onChange: (date: string) => console.log(date),
+    color: "secondary",
+  },
+};
