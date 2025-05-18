06edd8494bfebb0bc55aac33a8ef8e84f1f28649
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-05-18 22:26:47 +0530

    Select Component Exported

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Card } from "./Card";
 export { default as DatePicker } from "./DatePicker";
 export { default as Input } from "./Input";
 export { default as Pagination } from "./Pagination";
+export { default as Select } from "./Select";
